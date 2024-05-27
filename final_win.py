8b740dec082bc2c3c100881fa3b2d3d18a726489
Ayanakoji7878 <169060301+Ayanakoji7878@users.noreply.github.com>
2024-05-27 15:33:21 +0300

    Add files via upload

--- final_win.py
+++ final_win.py
@@ -1,1 +1,24 @@
-# напиши здесь код третьего экрана приложения
+# напиши здесь код третьего экрана приложения 3
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont 
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit
+      
+class FinalWin(QWidget):
+    def __init__(self):
+        self.initUI()
+        self.set_appear()
+        self.show()
+    def initUI(self):
+    self.workh_name = QLabel(txt_workheart)
+    self.index_text = QLabel(txt_index)
+    self.layout_line = QVBoxLayout()
+    self.layout_line.addWidget(self.index_text, alignment=Qt.AlignCenter)
+    self.setLayout(self.layout_line)
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width, win_height)
+        self.move(win_x, win_y)
